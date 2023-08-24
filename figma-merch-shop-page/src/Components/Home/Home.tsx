07775a99f077624cea87200f0b928d5634124d80
BCAndreaC <bc.andreac@gmail.com>
2023-08-24 16:21:22 -0500

    feat: configuration

---
+++ figma-merch-shop-page/src/Components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Título</h1>
+      <p>Este es un párrafo.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
